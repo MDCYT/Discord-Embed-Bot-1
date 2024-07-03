7e9a2d70a817e364f5b9f9c473c7c90e33c2e2e9
MDCDEV <me@mdcdev.me>
2024-07-03 13:10:41 -0500

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -99,10 +99,6 @@ module.exports = {
 				'\\$&',
 			)})\\s*`,
 		);
-		console.log(message.content)
-		console.log((message.mentions.users.has(message.client.id) || message.mentions.users.has(client.id)))
-		console.log((process.env.AI_CHANNEL.split(',').includes(message.channel.id) || process.env.AI_CHANNEL.split(',').includes(message.channel.parentId)))
-		console.log((message.mentions.users.has(message.client.id) || message.mentions.users.has(client.id)) && (process.env.AI_CHANNEL.split(',').includes(message.channel.id) || process.env.AI_CHANNEL.split(',').includes(message.channel.parentId)))
 
 		if (prefixRegex.test(message.content)) {
 			const [, match] = message.content.match(prefixRegex);
